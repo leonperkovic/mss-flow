e8c12b686134f27eb7ccceb4abeeef6a2f96a6a9
leon.perkovic <leon.perkovic@beeline.com>
2024-05-21 15:31:56 +0200

    Introduce compensate task

--- src/main/java/mars/mss/flow/delegate/RentSpaceshipDelegate.java
+++ src/main/java/mars/mss/flow/delegate/RentSpaceshipDelegate.java
@@ -15,7 +15,6 @@ public class RentSpaceshipDelegate implements JavaDelegate {
         this.mssClient = mssClient;
     }
 
-
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         var crewSize = (Integer) execution.getVariable(ProcessVariables.CREW_SIZE);
@@ -23,7 +22,7 @@ public class RentSpaceshipDelegate implements JavaDelegate {
         var spaceships = mssClient.getSpaceships(true);
         var rentedSpaceship = ShipUtils.findClosestForCrewSize(crewSize, spaceships);
 
-        execution.setVariable(ProcessVariables.RENTED_SHIP_ID, rentedSpaceship.getId());
-        execution.setVariable(ProcessVariables.SHIP_CARGO_LIMIT, rentedSpaceship.getCargoLimit());
+        execution.setVariable(ProcessVariables.RENTED_SHIP_ID, rentedSpaceship != null ? rentedSpaceship.getId() : null);
+        execution.setVariable(ProcessVariables.SHIP_CARGO_LIMIT, rentedSpaceship != null ? rentedSpaceship.getCargoLimit() : null);
     }
 }
